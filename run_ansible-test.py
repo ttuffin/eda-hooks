12fc47aa4194f8e65488f187b8e84ae7591195ec
Thomas Tuffin <ttuffin@redhat.com>
2022-08-21 22:37:14 +0200

    Initial commit with first iteration of hook.

---
+++ run_ansible-test.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import argparse
+import subprocess
+
+from typing import Sequence
+
+
+def _run_ansible_test(command: str):
+    args = ["ansible-test", command, "--venv"]
+    result = subprocess.run(args)
+    return result.returncode
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('test_type', nargs=1 ,help="The type of test to run. Sanity or unit.")
+    args = parser.parse_args(argv)
+
+    retv = _run_ansible_test(args.test_type[0])
+    return retv
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
